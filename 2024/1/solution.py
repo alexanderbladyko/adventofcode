d5d45ad606321d39b756880ebec885be2dfc3a27
Alexandr Bladyko <a_bladyko@wargaming.net>
2024-12-02 11:24:02 +0100

    First and second tasks

--- 2024/1/solution.py
+++ 2024/1/solution.py
@@ -1,4 +1,31 @@
 # https://adventofcode.com/2024/day/1
 
-with open("input.txt", "r") as f:
-    lines = f.read()
+
+from pathlib import Path
+
+from collections import Counter
+
+p = Path(__file__).with_name("input.txt")
+with p.open("r") as f:
+    lines = f.readlines()
+
+measure_1 = []
+measure_2 = []
+
+for line in lines:
+    parts = line.split()
+    measure_1.append(int(parts[0]))
+    measure_2.append(int(parts[1]))
+
+
+solution_1 = sum(
+    abs(left - right) for left, right in zip(sorted(measure_1), sorted(measure_2))
+)
+
+print("Solution 1 - ", solution_1)
+
+measure_2_counts = Counter(measure_2)
+
+solution_2 = sum(m * measure_2_counts[m] for m in measure_1)
+
+print("Solution 2 - ", solution_2)
---
+++ 2024/2/solution.py
@@ -0,0 +1,42 @@
+# https://adventofcode.com/2024/day/1
+
+
+from pathlib import Path
+
+p = Path(__file__).with_name("input.txt")
+with p.open("r") as f:
+    lines = f.readlines()
+
+reports = []
+for line in lines:
+    reports.append([int(p) for p in line.split()])
+
+
+def is_save(report):
+    is_asc = report[0] < report[1]
+
+    for first, second in zip(report, report[1:]):
+        if is_asc and first > second:
+            return False
+        if not is_asc and first < second:
+            return False
+        diff = abs(first - second)
+        if diff == 0 or diff > 3:
+            return False
+    return True
+
+
+def is_save_without_one(report):
+    if is_save(report):
+        return True
+    for i in range(len(report)):
+        if is_save(report[:i] + report[i + 1 :]):
+            return True
+    return False
+
+
+solution_1 = sum(1 for report in reports if is_save(report))
+print("Solution 1 - ", solution_1)
+
+solution_2 = sum(1 for report in reports if is_save_without_one(report))
+print("Solution 2 - ", solution_2)
