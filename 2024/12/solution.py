699dbaccba58672590d05b189fd702fc678f9031
Alexandr Bladyko <sanechek1704@gmail.com>
2024-12-12 19:38:21 +0100

    Day 12

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2024/day/11
+# https://adventofcode.com/2024/day/12
 
 from pathlib import Path
 from collections import deque
@@ -18,6 +18,13 @@ DIRECTIONS = [
     (0, -1),
 ]
 
+CORNERS = [
+    (-1, -1),
+    (1, 1),
+    (-1, 1),
+    (1, -1),
+]
+
 
 def get_area(sx, sy):
     plant = lines[sx][sy]
@@ -29,11 +36,9 @@ def get_area(sx, sy):
         x, y = queue.pop()
         for dx, dy in DIRECTIONS:
             nx, ny = x + dx, y + dy
-            if (nx, ny) in visited:
-                continue
-            visited.add((nx, ny))
             if 0 <= nx < N and 0 <= ny < M:
-                if lines[nx][ny] == plant:
+                if lines[nx][ny] == plant and (nx, ny) not in visited:
+                    visited.add((nx, ny))
                     shape.add((nx, ny))
                     queue.append((nx, ny))
                 elif lines[nx][ny] != plant:
@@ -43,15 +48,34 @@ def get_area(sx, sy):
     return shape, perimeter
 
 
+def get_sides(shape):
+    sides = 0
+
+    for x, y in shape:
+        for dx, dy in CORNERS:
+            if (x + dx, y) not in shape and (x, y + dy) not in shape:
+                sides += 1
+
+            if (
+                (x, y + dy) in shape
+                and (x + dx, y) in shape
+                and (x + dx, y + dy) not in shape
+            ):
+                sides += 1
+
+    return sides
+
+
 solution_1 = 0
+solution_2 = 0
 visited = set()
 for i in range(N):
     for j in range(M):
         if (i, j) not in visited:
             shape, perimeter = get_area(i, j)
-            print(lines[i][j], shape, len(shape), perimeter)
             visited = visited.union(shape)
             solution_1 += len(shape) * perimeter
+            solution_2 += len(shape) * get_sides(shape)
 
 print("Solution 1 - ", solution_1)
-# print("Solution 2 - ", solution_2)
+print("Solution 2 - ", solution_2)
